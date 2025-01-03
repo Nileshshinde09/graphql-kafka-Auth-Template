f8f4230f6b92dcd74ff82e91d16d4e586219043a
Nileshshinde09 <nileshshindeofficial@gmail.com>
2025-01-03 18:47:28 +0530

    added some code

--- graphql-server/src/PubSub/bullmq/index.ts
+++ graphql-server/src/PubSub/bullmq/index.ts
@@ -3,3 +3,16 @@ import { redisConnection } from "../../db/redis";
 import { BullMQ_TOKEN_Enums } from "../../constants/Enums";
 import type { BullMQ_TOKEN_Enums as BullMQ_TOKEN_Enums_TYPE  } from "../../constants/Enums";
 
+export const notificationQueue = new Queue(BullMQ_TOKEN_Enums.SEND_NOTIFICATIONS,{
+    connection: redisConnection,
+});
+export const notificationWorker = new Worker(BullMQ_TOKEN_Enums.SEND_NOTIFICATIONS,{
+    connection: redisConnection,
+});
+
+export const authEmailQueue = new Queue(BullMQ_TOKEN_Enums.SEND_AUTH_EMAIL,{
+    connection: redisConnection,
+});
+export const authEmailWorker = new Worker(BullMQ_TOKEN_Enums.SEND_AUTH_EMAIL,{
+    connection: redisConnection,
+});
